ed2b05eca007e72033b52192174bb330e5d6a59d
Thanh Lanh <thanhlanh12a3@gmail.com>
2023-03-21 23:05:41 +0700

    add tailwind css & init component

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { UserComponent } from './user/user.component';
+import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    UserComponent,
+    LoginComponent,
+    NotFoundComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
