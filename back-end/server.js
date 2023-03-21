e2de5cb7fad55040d952568670d95133a5693da1
Thanh Lanh <thanhlanh12a3@gmail.com>
2023-03-22 00:35:28 +0700

    init base backend

---
+++ back-end/server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const port = 3000
+
+const httpReq = morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'), '-',
+      tokens['response-time'](req, res), 'ms'
+    ].join(' ')
+})
+
+app.use(
+    httpReq
+)
+
+app.get('/hello', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
