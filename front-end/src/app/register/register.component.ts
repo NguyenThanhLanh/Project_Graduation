998e127e703f78efa65eb02fe4b90c80f4bc86a7
Thanh Lanh <thanhlanh12a3@gmail.com>
2023-04-12 00:49:33 +0700

    header + footer

--- front-end/src/app/register/register.component.ts
+++ front-end/src/app/register/register.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, Validator, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  constructor(private builder: FormBuilder, private toastr: ToastrService){}
 
+  registerForm = this.builder.group({
+    id: this.builder.control('', Validators.compose([Validators.required, Validators.minLength(5)])),
+    name: this.builder.control('', Validators.required),
+    password: this.builder.control('', Validators.compose([Validators.required, Validators.pattern('')]))
+  })
 }
